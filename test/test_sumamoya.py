e271371d70103fa8d1b53b1008bd2748396b4e6d
freetime27 <imoya33@gmail.com>
2025-11-07 14:41:39 -0300

    Update test_sumamoya.py

--- test/test_sumamoya.py
+++ test/test_sumamoya.py
@@ -1,5 +1,5 @@
-#tests/test_sumar.py
 from funciones.sumamoya import sumar
+
 def test_sumar():
- assert sumar(3, 5) == 8
- assert sumar(-2, 2) == 0
+    assert sumar(3, 5) == 8
+    assert sumar(-2, 2) == 0
