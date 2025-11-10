71e0bd7aa120c3585057dbd2617ba525f39e699d
BogadoCA <alejandromercer.99@gmail.com>
2025-11-10 19:29:18 -0300

    intento numero x

--- funciones/factorialbogado.py
+++ funciones/factorialbogado.py
@@ -1,7 +1,11 @@
-#funciones/factorial.py 
-import math 
-def factorial(n): 
-"""Devuelve el factorial de un número entero no negativo.""" 
-if n < 0: 
-return None 
-return math.factorial(n)
+def factorial(n):
+    """Devuelve el factorial de un número entero no negativo."""
+    if n < 0:
+        raise ValueError("El número debe ser no negativo")
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        resultado = 1
+        for i in range(2, n + 1):
+            resultado *= i
+        return resultado
