39ddda14503a8d278df28d3ac36597d50f3ddc7b
Samsam9410 <sam.a.celis@gmail.com>
2025-11-10 09:52:16 -0300

    Update test_multiplicacion2celis.py

--- test/test_multiplicacion2celis.py
+++ test/test_multiplicacion2celis.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from funciones.multiplicacion2celis import multiplicar
 
 def test_multiplicar():
