34c9dafcab13542e795b950e0f2f3778e5620a10
BogadoCA <alejandromercer.99@gmail.com>
2025-11-10 19:12:21 -0300

    intento numero x

--- test/test_multiplicacioncelis.py
+++ test/test_multiplicacioncelis.py
@@ -1,4 +1,4 @@
-from funciones.multiplicacion import multiplicar
+from funciones.multiplicacioncelis import multiplicar
 def test_multiplicar():
  assert multiplicar(3, 4) == 12
  assert multiplicar(-2, 5) == -10
