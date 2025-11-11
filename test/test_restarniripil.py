d7553c430bea24addd71256273a5bfda3298bae3
Mayli92 <niripil.mailen@gmail.com>
2025-11-10 22:16:46 -0300

    actualizacion del import  en test_restarniripil.py para el modulo  restarniripil

--- test/test_restarniripil.py
+++ test/test_restarniripil.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from funciones.restar import restar
+from funciones.restarniripil import restar
 
 def test_restar(): 
     assert restar(10, 4) == 6 
