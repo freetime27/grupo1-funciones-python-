795df45c1221ce0820deac04bc7d3d0248f24b26
unknown <antonela_olmedo@yajoo.com.ar>
2025-11-10 22:44:09 -0300

    Actualizo funciones y tests de promedio

--- test/test_promedioolmedo.py
+++ test/test_promedioolmedo.py
@@ -1,4 +1,8 @@
 #test/test_promedio.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from funciones.promedioolmedo import promedio
 def test_promedio():
  assert promedio([2, 4, 6]) == 4
