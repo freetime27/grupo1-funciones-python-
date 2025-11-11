b20802a57c9c5ef05a5ebdb336007f985d4f0dcb
unknown <antonela_olmedo@yajoo.com.ar>
2025-11-10 23:00:41 -0300

    Actualizado

--- funciones/promedioolmedo.py
+++ funciones/promedioolmedo.py
@@ -1,6 +1,6 @@
-#funciones/promedio.py
 def promedio(numeros):
- """Devuelve el promedio de una lista de números."""
- if not numeros:
- return None
- return sum(numeros) / len(numeros)
+    """Devuelve el promedio de una lista de números."""
+    if not numeros:
+        return None
+    return sum(numeros) / len(numeros)
+
