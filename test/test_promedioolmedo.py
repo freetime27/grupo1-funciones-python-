ee8470dd02956a6aec201b59cbb3e54f54ad3b7d
unknown <antonela_olmedo@yajoo.com.ar>
2025-11-10 22:07:34 -0300

    Actualizo

--- test/test_promedioolmedo.py
+++ test/test_promedioolmedo.py
@@ -1,5 +1,5 @@
 #test/test_promedio.py
-from funciones.promedioolmedo import promedioolmedo
-def test_promedioolmedo():
- assert promedioolmedo([2, 4, 6]) == 4
- assert promedioolmedo([]) is None
+from funciones.promedioolmedo import promedio
+def test_promedio():
+ assert promedio([2, 4, 6]) == 4
+ assert promedio([]) is None
